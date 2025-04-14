83fa90cb093493b92e8c23dd5316a88a6f4e2fd5
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-04-14 17:14:06 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Web App123',
+      title: 'Flutter Web App1234',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
